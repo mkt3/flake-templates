15b3117669ee7af71e03271474d985b0db0bb082
Makoto Morinaga <makoto@mkt3.dev>
2024-08-20 10:11:43 +0900

    Allow unfree

--- minimal/flake.nix
+++ minimal/flake.nix
@@ -1,14 +1,23 @@
 {
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         inherit (nixpkgs.lib) optional;
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
       in
       {
         devShell = pkgs.mkShell {
@@ -16,5 +25,6 @@
             # pkgs.xxx
           ];
         };
-      });
+      }
+    );
 }
